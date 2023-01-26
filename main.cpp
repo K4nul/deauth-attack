1756d32b595a378c3c4843fa5e7f36caddb81b80
kojjok97 <kojjok971@naver.com>
2023-01-27 00:27:57 +0900

    [DeauthAttack][BoB과제]오타수정

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 	1-2. station Mac이 명시될 경우 Unicast로 공격 
 	1.3. -Auth 옵션 입력시 Deauthentiation가 아닌 Authentiation으로 공격
 
-2.각 공격 별 패킷 철 
+2.각 공격 별 패킷 처리 
 	1번 목적지 Mac은 Broadcast로 ap Mac은 입력받은 값으로 
 	2번 목적지 Mac은 입력받은 값으로 ap Mac은 입력받은 값으로 
 	3번 -auth 옵션 입력시 따로 함수 만들기 
